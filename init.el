d8cfdf7b9ac27915ff8004e338e0712565555cd8
Matthew Harm Bekkema <mbekkema97@gmail.com>
2017-09-16 23:32:49 +1000

    Import my config
    Assimilate dash            2.13.0
    Assimilate evil            1.2.12
    Assimilate evil-magit     v0.4.1
    Assimilate goto-chg        20131228.1559-g79fdd52
    Assimilate linum-relative  0.5
    Assimilate magit           2.11.0
    Assimilate solarized      v1.2.2
    Assimilate undo-tree       20170706.1046-ge9a9102
    Assimilate with-editor    v2.6.0

--- init.el
+++ init.el
@@ -1,3 +1,54 @@
+; Basic
 (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
 (require 'borg)
 (borg-initialize)
+(require 'evil-magit)
+
+(setq make-backup-files nil)
+(tool-bar-mode 0)
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+; Solarized
+(custom-set-variables
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+(load-theme 'solarized-dark)
+
+; Evil mode
+(evil-mode 1)
+
+(defun evil-keyboard-quit ()
+  "Keyboard quit and force normal state."
+  (interactive)
+  (and evil-mode (evil-force-normal-state))
+  (keyboard-quit))
+
+(define-key evil-normal-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-motion-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-insert-state-map   (kbd "C-g") #'evil-normal-state)
+(define-key evil-insert-state-map   [tab]       #'evil-normal-state)
+(define-key evil-window-map         (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-operator-state-map (kbd "C-g") #'evil-keyboard-quit)
+
+(define-key evil-normal-state-map ";" 'evil-ex)
+
+; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+; Linum relative
+(linum-relative-global-mode)
+(setq linum-relative-current-symbol "")
+
+; Whitespace
+(setq whitespace-display-mappings nil)
+
+(custom-set-faces
+  '(whitespace-tab ((t (:background "#073642"))))
+  '(whitespace-trailing ((t (:background "yellow"))))
+  )
+
+(setq whitespace-style (quote (face tabs tab-mark trailing)))
+
+(global-whitespace-mode)
