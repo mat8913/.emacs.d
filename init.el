79e7a4478adfd835222e7f2387f516f6cfbfcd74
Matthew Harm Bekkema <mbekkema97@gmail.com>
2017-09-18 00:13:26 +1000

    Change powerline colourscheme and format

--- init.el
+++ init.el
@@ -57,26 +57,36 @@
 (custom-set-faces
  '(mode-line ((t (:overline nil :underline nil))))
  '(mode-line-inactive ((t (:overline nil :underline nil))))
+ '(powerline-evil-normal-face ((t (:inherit powerline-evil-base-face :background "#AFD700" :foreground "#005F00" :weight bold))))
+ '(powerline-evil-insert-face ((t (:inherit powerline-evil-base-face :background "#FFFFFF" :foreground "#005F5F" :weight bold))))
+ '(powerline-evil-visual-face ((t (:inherit powerline-evil-base-face :background "#FFAF00" :foreground "#875F00" :weight bold))))
+ '(powerline-evil-replace-face ((t (:inherit powerline-evil-base-face :background "#D70000" :foreground "#FFFFFF" :weight bold))))
 )
 
 (defface my-pl-segment1-active
-  '((t (:foreground "#000000" :background "#E1B61A")))
+  '((t (:foreground "#FFFFFF" :background "#585858" :weight bold)))
   "Powerline first segment active face.")
 (defface my-pl-segment1-inactive
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  '((t (:foreground "#808080" :background "#262626")))
   "Powerline first segment inactive face.")
 (defface my-pl-segment2-active
-  '((t (:foreground "#F5E39F" :background "#8A7119")))
+  '((t (:foreground "#9E9E9E" :background "#303030")))
   "Powerline second segment active face.")
 (defface my-pl-segment2-inactive
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  '((t (:foreground "#4F4F4F" :background "#121212")))
   "Powerline second segment inactive face.")
 (defface my-pl-segment3-active
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  '((t (:foreground "#9E9E9E" :background "#585858")))
   "Powerline third segment active face.")
 (defface my-pl-segment3-inactive
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  '((t (:foreground "#626262" :background "#262626")))
   "Powerline third segment inactive face.")
+(defface my-pl-segment4-active
+  '((t (:foreground "#262626" :background "#D0D0D0")))
+  "Powerline fourth segment active face.")
+(defface my-pl-segment4-inactive
+  '((t (:foreground "#121212" :background "#626262")))
+  "Powerline fourth segment inactive face.")
 
 (defun air--powerline-default-theme ()
   "Set up my custom Powerline with Evil indicators."
@@ -87,6 +97,7 @@
                           (seg1 (if active 'my-pl-segment1-active 'my-pl-segment1-inactive))
                           (seg2 (if active 'my-pl-segment2-active 'my-pl-segment2-inactive))
                           (seg3 (if active 'my-pl-segment3-active 'my-pl-segment3-inactive))
+                          (seg4 (if active 'my-pl-segment4-active 'my-pl-segment4-inactive))
                           (separator-left (intern (format "powerline-%s-%s"
                                                           (powerline-current-separator)
                                                           (car powerline-default-separator-dir))))
@@ -94,10 +105,10 @@
                                                            (powerline-current-separator)
                                                            (cdr powerline-default-separator-dir))))
                           (lhs (list (let ((evil-face (powerline-evil-face)))
-                                       (if evil-mode
+                                       (if (and active evil-mode)
                                            (powerline-raw (powerline-evil-tag) evil-face)
                                          ))
-                                     (if evil-mode
+                                     (if (and active evil-mode)
                                          (funcall separator-left (powerline-evil-face) seg1))
                                      (powerline-buffer-id seg1 'l)
                                      (powerline-raw "[%*]" seg1 'l)
@@ -105,6 +116,11 @@
                                        (powerline-raw which-func-format seg1 'l))
                                      (powerline-raw " " seg1)
                                      (funcall separator-left seg1 seg2)
+                                     (powerline-vc seg2 'r)
+                                     ))
+                          (rhs (list (powerline-raw global-mode-string seg2 'r)
+
+
                                      (when (boundp 'erc-modified-channels-object)
                                        (powerline-raw erc-modified-channels-object seg2 'l))
                                      (powerline-major-mode seg2 'l)
@@ -112,24 +128,22 @@
                                      (powerline-minor-modes seg2 'l)
                                      (powerline-narrow seg2 'l)
                                      (powerline-raw " " seg2)
-                                     (funcall separator-left seg2 seg3)
-                                     (powerline-vc seg3 'r)
-                                     (when (bound-and-true-p nyan-mode)
-                                       (powerline-raw (list (nyan-create)) seg3 'l))))
-                          (rhs (list (powerline-raw global-mode-string seg3 'r)
-                                     (funcall separator-right seg3 seg2)
+
+                                     (funcall separator-right seg2 seg3)
+
                                      (unless window-system
-                                       (powerline-raw (char-to-string #xe0a1) seg2 'l))
-                                     (powerline-raw "%4l" seg2 'l)
-                                     (powerline-raw ":" seg2 'l)
-                                     (powerline-raw "%3c" seg2 'r)
-                                     (funcall separator-right seg2 seg1)
-                                     (powerline-raw " " seg1)
-                                     (powerline-raw "%6p" seg1 'r)
+                                       (powerline-raw (char-to-string #xe0a1) seg3 'l))
+                                     (powerline-raw " " seg3)
+                                     (powerline-raw "%6p" seg3 'r)
+                                     (funcall separator-right seg3 seg4)
+                                     (powerline-raw "%4l" seg4 'l)
+                                     (powerline-raw ":" seg4 'l)
+                                     (powerline-raw "%3c" seg4 'r)
                                      (when powerline-display-hud
-                                       (powerline-hud seg1 seg3)))))
+                                       (powerline-hud seg4 seg3))
+                                     )))
                      (concat (powerline-render lhs)
-                             (powerline-fill seg3 (powerline-width rhs))
+                             (powerline-fill seg2 (powerline-width rhs))
                              (powerline-render rhs)))))))
 
 (setq powerline-default-separator (if (display-graphic-p) 'arrow
