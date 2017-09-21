mat8913/.emacs.d

commit cd353f4857ce87d69fa3256949b4f8f75774fdab

Make line numbers more stylish

init.el
@@ -41,6 +41,13 @@
 (linum-relative-global-mode)
 (setq linum-relative-current-symbol "")
 
+(defadvice linum-update-window (around linum-dynamic activate)
+  (let* ((w (length (number-to-string
+                     (count-lines (point-min) (point-max)))))
+         (linum-format (concat " %" (number-to-string w) "d ")))
+    ad-do-it))
+(set-face-attribute 'linum nil :background "#073642" :weight 'normal)
+
 ; Whitespace
 (setq whitespace-display-mappings nil)
 

commit a2a3613c7d9c3b8b0fc851f73a638ccad83fde4c

Fix linum-relative
I accidently broke it in cd353f4857ce87d69fa3256949b4f8f75774fdab

init.el
@@ -44,7 +44,7 @@
 (defadvice linum-update-window (around linum-dynamic activate)
   (let* ((w (length (number-to-string
                      (count-lines (point-min) (point-max)))))
-         (linum-format (concat " %" (number-to-string w) "d ")))
+         (linum-relative-format (concat " %" (number-to-string w) "s ")))
     ad-do-it))
 (set-face-attribute 'linum nil :background "#073642" :weight 'normal)
 